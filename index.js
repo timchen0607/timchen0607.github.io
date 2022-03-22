f9399c1a610c395894ce8e4ef6e68b68509b8067
Tim School <a0963573232@gmail.com>
2022-03-22 19:36:55 +0800

    Feats: 新增導覽列

--- index.js
+++ index.js
@@ -14,17 +14,30 @@ document.addEventListener("alpine:init", () => {
       this.skillsBackEndIdx = 1;
       this.skillsOthersIdx = 1;
       this.blogsIdx = 1;
-      this.skillsParallax();
       this.fetchProfile()
         .then(() => this.fetchPortfolio())
         .then(() => this.fetchSkills())
         .then(() => this.fetchCertificate())
         .then(() => this.fetchBlogs())
         .then(() => {
-          document.documentElement.style.overflow = "auto";
-          document.body.style.overflow = "auto";
+          this.autoActive();
+          this.skillsParallax();
+          document.addEventListener("scroll", () => this.autoActive());
+          document.documentElement.style.overflow = "visible";
+          document.body.style.overflow = "visible";
           document.getElementById("loading").classList.add("hide");
         });
+    },
+    autoActive() {
+      let current = "";
+      this.$el.querySelectorAll("section").forEach((sec) => {
+        if (window.pageYOffset >= sec.offsetTop - 60)
+          current = sec.getAttribute("id");
+      });
+      this.$el.querySelectorAll(".navbar-item").forEach((item) => {
+        item.classList.remove("active");
+        if (item.href.includes(current)) item.classList.add("active");
+      });
     },
     fetchProfile() {
       return fetch("https://timchen0607.github.io/api/profile.json")
